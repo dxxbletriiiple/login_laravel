9e9961271125296a8e430988337b3f7025b649b7
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-03-16 05:14:50 +0300

    resolve problems

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
+    @vite('resources/css/app.css')
 </head>
 
 <body class="antialiased">
